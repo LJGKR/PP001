ca7c3370033183e662f7b436290ffcb3bd99e73f
진규 이 <wlsrb9086@naver.com>
2022-05-15 18:46:24 +0900

    [JG] 윈도우창 공룡게임 초기구현

--- PP006/main.cpp
+++ PP006/main.cpp
@@ -18,9 +18,22 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GL_TRUE);
 }
+
 int main(void)
 {
 
+    float h1 = 0.0f;
+    float h2 = -0.04f;
+
+
+    float x1 = 0.8f;
+    float x2 = 0.83f;
+
+    float x3 = 0.8f;
+    float x4 = 0.83f;
+
+    float x5 = 0.8f;
+    float x6 = 0.83f;
 
     GLFWwindow* window;
     glfwSetErrorCallback(error_callback);
@@ -35,65 +48,136 @@ int main(void)
     glfwMakeContextCurrent(window);
     glfwSetKeyCallback(window, key_callback);
 
+    
+
     while (!glfwWindowShouldClose(window))
     {
-        float h1 = 0.0f;
-        float h2 = -0.04f;
+    
         float ratio;
         int width, height;
         glfwGetFramebufferSize(window, &width, &height);
         ratio = width / (float)height;
 
+        
 
-        glClearColor(1, 1, 1, 1);
+
+        glClearColor(0, 0, 0, 0);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        /*glPointSize(10);
 
-        glBegin(GL_POINTS);
+        if (GetAsyncKeyState(VK_SPACE) && -0x8000 || GetAsyncKeyState(VK_SPACE) && -0x8001)        //h1 = 0.0f      h2 = -0.04f
+        {
+            h1 += 0.007f;
+            h2 += 0.007f;
+        }
+        else
+        {
+            h1 -= 0.007f;
+            h2 -= 0.007f;
+        }
 
-        glColor3f(1.0f, 0.0f, 0.0f);
-        glVertex2f(0.0f, 0.0f);
+        if (h1 >= 0.1f && h2 >= 0.1f)
+        {
+            h1 = 0.1f;
+            h2 = 0.06f;
+        }
 
-        glEnd();*/
+        if (h1 <= -0.1f && h2 <= -0.1f)
+        {
+            h1 = -0.1f;
+            h2 = -0.14f;
+        }
 
+        
         glPointSize(5);
         glBegin(GL_TRIANGLES);
 
         glColor3f(1.0f, 0.0f, 0.0f);
-        glVertex2f(0.0f, h1);
+        glVertex2f(-0.8f, h1);
         glColor3f(1.0f, 0.0f, 0.0f);
-        glVertex2f(0.03f, h1);
+        glVertex2f(-0.83f, h1);
         glColor3f(1.0f, 0.0f, 0.0f);
-        glVertex2f(0.03f, h2);
+        glVertex2f(-0.83f, h2);
         
         glEnd();
 
         glPointSize(5);
+
         glBegin(GL_TRIANGLES);
 
         glColor3f(1.0f, 0.0f, 0.0f);
-        glVertex2f(0.0f, h1);
+        glVertex2f(-0.8f, h1);
         glColor3f(1.0f, 0.0f, 0.0f);
-        glVertex2f(0.0f, h2);
+        glVertex2f(-0.8f, h2);
         glColor3f(1.0f, 0.0f, 0.0f);
-        glVertex2f(0.03f, h2);
+        glVertex2f(-0.83f, h2);
 
         glEnd();
 
+        glPointSize(5);
+
+        glBegin(GL_TRIANGLES);
+
+        glColor3f(1.0f, 0.0f, 0.0f);
+        glVertex2f(x1, -0.04f);
+        glColor3f(1.0f, 0.0f, 0.0f);
+        glVertex2f(x2, -0.04f);
+        glColor3f(1.0f, 0.0f, 0.0f);
+        glVertex2f(x2, 0.0f);
+
+        glEnd();
+
+        glPointSize(5);
+
+        glBegin(GL_TRIANGLES);
+
+        glColor3f(1.0f, 0.0f, 0.0f);
+        glVertex2f(x3, -0.14f);
+        glColor3f(1.0f, 0.0f, 0.0f);
+        glVertex2f(x4, -0.14f);
+        glColor3f(1.0f, 0.0f, 0.0f);
+        glVertex2f(x3, -0.1f);
+
+        glEnd();
+
+        glPointSize(5);
+
+        glBegin(GL_TRIANGLES);
+
+        glColor3f(1.0f, 0.0f, 0.0f);
+        glVertex2f(x5, 0.04f);
+        glColor3f(1.0f, 0.0f, 0.0f);
+        glVertex2f(x6, 0.04f);
+        glColor3f(1.0f, 0.0f, 0.0f);
+        glVertex2f(x6, 0.0f);
+
+        glEnd();
+
+        x1 = x1 - 0.007f;
+        x2 = x2 - 0.007f;
+
+        x3 = x3 - 0.01f;
+        x4 = x4 - 0.01f;
+
+        x5 = x5 - 0.005f;
+        x6 = x6 - 0.005f;
+
+        if (x1 <= -1.2f && x2 <= -1.17f)
+        {
+            x1 = 0.8f;
+            x2 = 0.83f;
+        }else if (x3 <= -1.2f && x4 <= -1.17f)
+        {
+            x3 = 0.8f;
+            x4 = 0.83f;
+        }else if (x5 <= -1.2f && x6 <= -1.17f)
+        {
+            x5 = 0.8f;
+            x6 = 0.83f;
+        }
+
         glfwSwapBuffers(window);
         glfwPollEvents();
-
-        if (GetAsyncKeyState(VK_SPACE) && -0x8000 || GetAsyncKeyState(VK_SPACE) && -0x8001)        //h1 = 0.0f      h2 = -0.04f
-        {
-            h1 = 0.2f;
-            h2 = 0.16f;
-        }
-        else
-        {
-            h1 = 0.0f;
-            h2 = -0.04f;
-        }
     }
     glfwDestroyWindow(window);
     glfwTerminate();
